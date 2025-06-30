397985a4387958d91f363ec29e1255b5b4e605f0
Miguel luis <126804912+Miguelluisdev@users.noreply.github.com>
2025-06-30 17:06:58 -0300

    Add chat feature Cypress tests and image fixture
    Introduces a new test suite for user chat functionality in OrangeHRM using Cypress. Includes tests for posting text and posting text with an image, along with a sample image fixture for upload testing.

---
+++ OrangeHRM/cypress/e2e/Cenário 05-Chat.cy.js
@@ -0,0 +1,49 @@
+describe("Cenário 05: Chat dos usuários", () => {
+  beforeEach(() => {
+    cy.login();
+  });
+
+
+  it("Caso de teste 01 - Fazendo post apenas com texto", () => {
+    cy.contains("Buzz").click();
+    cy.url().should("include", "/buzz/viewBuzz");
+
+    const texto = "olá mundo , estou digitando ORangeHRM";
+
+    cy.get(".oxd-buzz-post-input").type(texto);
+    cy.get(".oxd-buzz-post-slot > .oxd-button").click();
+    cy.get("#oxd-toaster_1").should("be.visible");
+
+
+  });
+
+  it("Caso de teste 02 - Fazendo post com texto e imagem", () => {
+    cy.clock();
+    const texto = "olá , ORangeHRM";
+
+    cy.fixture("imagem-teste.png").as("myFile");
+
+    cy.contains("Buzz").click();
+    cy.url().should("include", "/buzz/viewBuzz");
+
+    cy.contains("Share Photos").click();
+
+    cy.get(".oxd-text--card-title").should("be.visible");
+
+    cy.get(".oxd-buzz-post-input").last().type(texto);
+
+    // Upload da imagem (forçando caso o campo esteja oculto)
+    cy.get('input[type="file"]').selectFile(
+      "cypress/fixtures/imagem-teste.png",
+      {
+        force: true,
+      }
+    );
+
+    cy.tick(2000);
+    cy.get(".oxd-form-actions > .oxd-button").click();
+    cy.tick(2000);
+
+
+  });
+});
