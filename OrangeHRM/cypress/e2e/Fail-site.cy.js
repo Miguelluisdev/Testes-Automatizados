e89e3690cccb66bc5fea398f7b569b5c2ea6ccdf
Miguel luis <126804912+Miguelluisdev@users.noreply.github.com>
2025-06-30 15:51:50 -0300

    feat: verificação de site fora do ar

---
+++ OrangeHRM/cypress/e2e/Fail-site.cy.js
@@ -0,0 +1,15 @@
+it("fail site", () => {
+ cy.verificarDisponibilidade();
+
+ cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+  cy.get("body").then(($body) => {
+    if ($body.find('input[name="username"]').length === 0) {
+      cy.log(
+        "⚠️ O site parece estar fora do ar ou não renderizou a interface corretamente."
+      );
+      Cypress.runner.stop(); 
+    } else {
+      cy.log("✅ Interface carregada com sucesso.");
+    }
+  });
+});
