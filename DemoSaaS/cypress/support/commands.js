d39c742a9dfb4f663f4a5f0372cf9c51fc70952a
Miguel luis <126804912+Miguelluisdev@users.noreply.github.com>
2025-07-04 15:35:27 -0300

    feat: Add organization creation Cypress tests and page object
    Introduces end-to-end tests for organization creation using Cypress, including happy path, negative, and alternative scenarios. Adds a new page object for organization-related actions and updates custom commands to include a login helper. Also adds @faker-js/faker as a dev dependency for generating test data.

--- DemoSaaS/cypress/support/commands.js
+++ DemoSaaS/cypress/support/commands.js
@@ -1,4 +1,4 @@
-Cypress.Commands.add("cadastrarUsuario", (user) => {
+Cypress.Commands.add("cadastro", (user) => {
   cy.contains("Go to example sign up").click();
 
   cy.url().should("include", "/signup");
@@ -10,6 +10,18 @@ Cypress.Commands.add("cadastrarUsuario", (user) => {
   cy.get('input[name="email"]').type(user.email);
 
   cy.get('input[name="password"]').type(user.password);
+
+  cy.get(".mantine-Fieldset-root > .m_6d731127 > .m_77c9d27d").click();
+});
+
+Cypress.Commands.add("login", () => {
+  cy.visit("https://demo-saas.bugbug.io/sign-in");
+
+  cy.url().should("include", "/sign-in");
+
+  cy.get('input[name="email"]').type('miguelluisatf@gmail.com');
+
+  cy.get('input[name="password"]').type('XyzioSCtuq');
 
   cy.get('.mantine-Fieldset-root > .m_6d731127 > .m_77c9d27d').click();
 });
---
+++ DemoSaaS/cypress/support/pageObjects/CT_RF03-Page.js
@@ -0,0 +1,43 @@
+class OrganizationPage {
+  button() {
+    cy.contains("Create new organization").click();
+  }
+
+  fillName(name) {
+    cy.get('input[name="name"]').type(name);
+  }
+
+  dashboardVisible() {
+    cy.get(".m_e615b15f").should("be.visible");
+  }
+
+  urlDashboard() {
+    cy.url().should("include", "/onboarding");
+  }
+
+  urlCreateOrg() {
+    cy.url().should("include", "/create-organization");
+  }
+
+  
+
+  createButton() {
+    cy.get(".m_811560b9").click();
+  }
+
+  errorMessage() {
+    cy.contains("String must contain at least 3 character(s)").should(
+      "have.text",
+      "String must contain at least 3 character(s)"
+    );
+  }
+
+  checkInput() {
+    cy.get('input[name="importExampleTickets"]')
+      .should("exist")
+      .check({ force: true })
+      .should("be.checked");
+  }
+}
+
+export default new OrganizationPage();
