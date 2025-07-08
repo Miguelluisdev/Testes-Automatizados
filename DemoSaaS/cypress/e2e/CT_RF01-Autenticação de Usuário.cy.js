1123484508da6089927c939b822984f0243b6de4
Miguel luis <126804912+Miguelluisdev@users.noreply.github.com>
2025-07-08 03:11:50 -0300

    refactor: Inline login credentials in authentication tests
    Removed the loginUser object and directly inlined email and password values in the test cases for user authentication. This simplifies the test code and improves readability.

--- DemoSaaS/cypress/e2e/CT_RF01-Autenticação de Usuário.cy.js
+++ DemoSaaS/cypress/e2e/CT_RF01-Autenticação de Usuário.cy.js
@@ -6,14 +6,9 @@ describe("Cenário 01: Autenticação de Usuário.", () => {
     cy.url().should("include", "/sign-in");
   });
 
-  const loginUser = {
-    email: "miguelluisatf@gmail.com",
-    password: "XyzioSCtuq",
-  };
-
   it("Caso de Teste 01: Login com as credenciais válidas (Happy Path)", () => {
-    LoginPage.fillEmail(loginUser.email);
-    LoginPage.fillPassword(loginUser.password);
+    LoginPage.fillEmail("miguelluisatf@gmail.com");
+    LoginPage.fillPassword("XyzioSCtuq");
     LoginPage.submit();
 
     cy.get('[data-testid="user-settings"] > p').should('be.visible');
@@ -21,14 +16,14 @@ describe("Cenário 01: Autenticação de Usuário.", () => {
 
   it("Caso de Teste 02: Login com credenciais de email inválidas (Teste Negativo)", () => {
     LoginPage.fillEmail("miguelmail.c");
-    LoginPage.fillPassword(loginUser.password);
+    LoginPage.fillPassword("XyzioSCtuq");
     LoginPage.submit();
 
     cy.contains("Invalid email").should("be.visible");
   });
 
   it("Caso de Teste 03: Login com credenciais de senha inválidas (Teste Negativo)", () => {
-    LoginPage.fillEmail(loginUser.email);
+    LoginPage.fillEmail("miguelluisatf@gmail.com");
     LoginPage.fillPassword("4g4");
     LoginPage.submit();
 
