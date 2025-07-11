615f855abbba79087ddcd614a111c5d04bb2b800
Miguel luis <126804912+Miguelluisdev@users.noreply.github.com>
2025-07-11 18:02:41 -0300

    update: login command to use static credentials
    Replaces use of environment variables for username and password with hardcoded values ('standard_user' and 'secret_sauce') in the Cypress login command.

--- SwagLabs/cypress/support/commands.js
+++ SwagLabs/cypress/support/commands.js
@@ -1,7 +1,7 @@
 Cypress.Commands.add("login", () => {
   cy.visit("https://www.saucedemo.com/v1/");
-  cy.get('[data-test="username"]').type(Cypress.env('username'));
-  cy.get('[data-test="password"]').type(Cypress.env('password'));
+  cy.get('[data-test="username"]').type("standard_user");
+  cy.get('[data-test="password"]').type("secret_sauce");
   cy.get("#login-button").click();
   cy.get(".product_label").should("be.visible");
   cy.log("login com dados validos (happy path)");
