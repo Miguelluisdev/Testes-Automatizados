88aa129c1099df99987bd76cd666b0994b002873
Miguel luis <126804912+Miguelluisdev@users.noreply.github.com>
2025-07-13 22:16:08 -0300

    feat: Add Cucumber and Allure support to Cypress config
    Integrates Cucumber preprocessor and Allure reporting into PrestaShop Cypress setup. Updates config, support files, and adds PrestaShop/package.json with relevant scripts and dependencies for enhanced BDD and reporting capabilities.

--- PrestaShop/cypress.config.js
+++ PrestaShop/cypress.config.js
@@ -1,7 +1,25 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
+const createEsbuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+
+module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    specPattern: "cypress/e2e/**/*.feature",
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      allureWriter(on, config);
+      return config;
     },
   },
-};
+});
