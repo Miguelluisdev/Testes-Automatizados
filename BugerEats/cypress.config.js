904be159ab350e4223693e5c7344f334d3b28f7e
Miguel luis <126804912+Miguelluisdev@users.noreply.github.com>
2025-07-14 18:04:23 -0300

    Integrate Cucumber and Allure plugins with Cypress
    Added support for Cucumber preprocessor and Allure reporting in Cypress by updating configuration and dependencies. Removed default custom commands template and imported Allure plugin in e2e support file.

--- BugerEats/cypress.config.js
+++ BugerEats/cypress.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
-  chromeWebSecurity: false,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+      on("file:preprocessor", createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      }));
+      allureWriter(on, config);
+      return config;
     },
   },
 });
