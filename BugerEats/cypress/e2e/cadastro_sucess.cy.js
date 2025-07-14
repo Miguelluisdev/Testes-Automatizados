261fef05272aedd8d51f9e90573fe092bf37c788
Miguel luis <126804912+Miguelluisdev@users.noreply.github.com>
2025-07-14 19:29:26 -0300

    feat: Add cadastro selectors and improve cadastro test
    Introduced a selectors module for cadastro form fields and enhanced the cadastro_sucess.cy.js test to use dynamic data via faker. Also added @faker-js/faker as a dev dependency and configured jsconfig.json for Cypress support.

--- BugerEats/cypress/e2e/cadastro_sucess.cy.js
+++ BugerEats/cypress/e2e/cadastro_sucess.cy.js
@@ -1,5 +1,38 @@
-describe('cenario 01: Cadastre-se para fazer entregas', () => {
-  it('Cadastre-se para fazer entregas', () => {
-    cy.visit('https://buger-eats-qa.vercel.app/')
-  })
-})
+import { nome, cpf, email, whatsapp } from "../support/selectors/cadastro";
+import { faker } from "@faker-js/faker";
+
+describe("Cenario 01: Cadastre-se para fazer entregas", () => {
+  beforeEach(() => {
+    cy.visit("https://buger-eats-qa.vercel.app/");
+    cy.contains("a", "Cadastre-se para fazer entregas")
+      .should("be.visible")
+      .click();
+    cy.url().should("include", "/deliver");
+  });
+
+  it("Cadastre-se para fazer entregas", () => {
+    const usuario = {
+      nomeFake: faker.person.fullName(),
+      cpfFake: "50837391865",
+      emailFake: faker.internet.email(),
+      whatsappFake: faker.phone.number("9754031002"),
+    };
+
+    cy.get(nome).type(usuario.nomeFake).should("have.value", usuario.nomeFake);
+    cy.get(cpf).type(usuario.cpfFake).should("have.value", usuario.cpfFake);
+    cy.get(email)
+      .type(usuario.emailFake)
+      .should("have.value", usuario.emailFake);
+    cy.get(whatsapp)
+      .type(usuario.whatsappFake)
+      .should("have.value", usuario.whatsappFake);
+  });
+
+  it("Validar campos obrigatórios", () => {
+    cy.get(nome).should("be.visible").and("have.value", "");
+    cy.get(cpf).should("be.visible").and("have.value", "");
+    cy.get(email).should("be.visible").and("have.value", "");
+    cy.get(whatsapp).should("be.visible").and("have.value", "");
+    cy.get(".button-success").should('be.visible').click();
+  });
+});
---
+++ BugerEats/cypress/support/selectors/cadastro.js
@@ -0,0 +1,4 @@
+export const nome = 'input[name="fullName"]'
+export const cpf = 'input[name="cpf"]'
+export const email = 'input[name="email"]'
+export const whatsapp = 'input[name="whatsapp"]'
