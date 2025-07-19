06efca16d7e90e6a268238ea978b76c1fce9cd99
Miguel luis <126804912+Miguelluisdev@users.noreply.github.com>
2025-07-19 16:31:32 -0300

    config: add new config for cypress and add cucumber dependencies

--- DemoSaaS/cypress.config.js
+++ DemoSaaS/cypress.config.js
@@ -1,9 +1,38 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const fs = require("fs-extra");
+
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: "https://demo-saas.bugbug.io/",
+    env: {
+      username: process.env.NAME,
+      password: process.env.PASSWORD,
+    },
+    specPattern: ["cypress/e2e/**/*.feature", "cypress/e2e/**/*.cy.{js,ts}"],
+    supportFile: "cypress/support/e2e.js",
+
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      on("before:run", () => {
+        fs.emptyDirSync("cypress/sessions");
+      });
+
+      return config;
     },
   },
 });
